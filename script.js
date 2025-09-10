8bd12c70f19359e20d43b96c0d9ba5ea5d01d1e8
BLACKGAMINGSHOP <nn1149676@gmail.com>
2025-09-10 08:08:10 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Page Load Greeting
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("BLACK GAME SHOP Website Loaded 🎮");
+});
+
+// Example Interactive Button
+function showComingSoon() {
+  alert("🛒 Shop feature coming soon! Stay tuned...");
+}
